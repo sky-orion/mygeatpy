86cb4bf66bf3ceb20692f4069ee3f8228d63034a
hhaoran <55023820+sky-orion@users.noreply.github.com>
2021-11-08 20:05:34 +0800

    修改spea2bug

--- spea2.py
+++ spea2.py
@@ -16,22 +16,19 @@ class moea_SPEA2_templet(ea.MoeaAlgorithm):
             raise RuntimeError('传入的种群对象必须是单染色体的种群类型。')
         self.name = 'SPEA2'
         if population.Encoding == 'P':
-            self.recOper = ea.Xovpmx(XOVR=1, Half_N=False)  # 生成部分匹配交叉算子对象
+            self.recOper = ea.Xovpmx(XOVR=1)  # 生成部分匹配交叉算子对象
             self.mutOper = ea.Mutinv(Pm=1)  # 生成逆转变异算子对象
         elif population.Encoding == 'BG':
-            self.recOper = ea.Xovud(XOVR=1, Half_N=False)  # 生成均匀交叉算子对象
+            self.recOper = ea.Xovud(XOVR=1)  # 生成均匀交叉算子对象
             self.mutOper = ea.Mutbin(Pm=None)  # 生成二进制变异算子对象，Pm设置为None时，具体数值取变异算子中Pm的默认值
         elif population.Encoding == 'RI':
-            self.recOper = ea.Recsbx(XOVR=1, n=20, Half_N=False)  # 生成模拟二进制交叉算子对象
+            self.recOper = ea.Recsbx(XOVR=1, n=20)  # 生成模拟二进制交叉算子对象
             self.mutOper = ea.Mutpolyn(Pm=1 / self.problem.Dim, DisI=20)  # 生成多项式变异算子对象
         else:
             raise RuntimeError('编码方式必须为''BG''、''RI''或''P''.')
         self.N_ = None
         self.archive = None
         self.selFunc = 'tour'  # 选择方式，采用锦标赛选择
-        self.flush=True
-
-
 
     def Fitnessassignment(self, population, k):
         objective_values = population.ObjV
@@ -47,33 +44,29 @@ class moea_SPEA2_templet(ea.MoeaAlgorithm):
         R = S * dominated
         R = R.sum(axis=1)
         distances = (objective_values[:, np.newaxis, :] - objective_values[np.newaxis, :, :]) ** 2
-        # distances = distances.sum(axis=2)
-        # distances = np.sqrt(distances)
         distances = np.sort(np.sqrt(distances.sum(axis=2)), axis=1)
         k_distance = distances[:, k]
         D = 1 / (k_distance + 2)
         # Fitness
-        return  D.reshape((-1,1)) + R.reshape((-1,1))
+        return D.reshape((-1, 1)) + R.reshape((-1, 1))
 
     def EnvironmentalSelection(self, totalpopulation, N_archive):
         objective_values = totalpopulation.ObjV
         F = totalpopulation.FitnV
         indices_next_archive = np.where(F < 1)[0]
         length = len(indices_next_archive)
-        # print(length,N_archive)
+        # print(F,length,N_archive,F.shape,np.argsort(F.reshape(-1)))
         if length < N_archive:
-            best_inidices = np.argsort(F)
+            best_inidices = np.argsort(F.reshape(-1),kind="mergesort")
             indices_next_archive = best_inidices[:N_archive]
         elif length > N_archive:
             rest = length - N_archive
             dist = (objective_values[[indices_next_archive], np.newaxis, :] - objective_values[np.newaxis,
                                                                               [indices_next_archive], :]) ** 2
-            dist = np.squeeze(dist)
-            dist = np.sum(dist, axis=2)
-            dist = np.sqrt(dist)
+            dist = np.sqrt(np.sum(np.squeeze(dist), axis=2))
             ind = np.triu_indices(dist.shape[0])
-            dist[ind] = 10000
-            y, _ = np.unravel_index(np.argsort(dist.ravel()), dist.shape)
+            dist[ind] = 1000000
+            y, _ = np.unravel_index(np.argsort(dist.ravel(),kind="mergesort"), dist.shape)
             _, i = np.unique(y, return_index=True)
             indices_to_delete = y[sorted(i)][:rest]
             indices_next_archive = np.delete(indices_next_archive, indices_to_delete)
@@ -81,13 +74,12 @@ class moea_SPEA2_templet(ea.MoeaAlgorithm):
 
     def terminated(self, pop):  # 判断是终止进化，pop为当代种群对象
         self.stat(pop)  # 进行统计分析，更新进化记录器
-        if self.log['spacing'][-1]==0 and  self.flush:
-            return  False
-        if self.currentGen + 1 >= self.MAXGEN :
+        if self.currentGen + 1 >= self.MAXGEN:
             return True
         else:
             self.currentGen += 1  # 进化代数+1
             return False
+
     def run(self, prophetPop=None):  # prophetPop为先知种群（即包含先验知识的种群）
         # ==========================初始化配置===========================
         population = self.population
@@ -110,7 +102,7 @@ class moea_SPEA2_templet(ea.MoeaAlgorithm):
                 totalpopulation = population
             else:
                 totalpopulation = population + self.archive
-            totalpopulation.FitnV=self.Fitnessassignment(totalpopulation, K)
+            totalpopulation.FitnV = self.Fitnessassignment(totalpopulation, K)
             self.archive = self.EnvironmentalSelection(totalpopulation, N_archive)
             offspring = self.archive[ea.selecting(self.selFunc, self.archive.FitnV, NIND)]
             # 对选出的个体进行进化操作
@@ -127,5 +119,7 @@ class moea_SPEA2_templet(ea.MoeaAlgorithm):
                 break
 
         return self.finishing(self.archive)  # 调用finishing完成后续工作并返回结果
-if  __name__ == '__main__':
+
+
+if __name__ == '__main__':
     help(ea.ndsortESS)
